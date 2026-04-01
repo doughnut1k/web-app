b50d80335facaf9976b380daed9302c796556e49
Sahatsawat Pueanfoong <sahatsawat.pue+skyaidev@skyai.co.th>
2026-04-01 17:38:08 +0700

    Update footer and header components to display user names dynamically

--- app/components/footer/page.tsx
+++ app/components/footer/page.tsx
@@ -1,7 +1,8 @@
+import data from "../../master_data/data";
 export default function Footer() {
   return (
     <footer className="py-10 text-center text-zinc-500 text-sm">
-      © 2026 Sahatsawat Pueanfoong. All rights reserved.
+      © 2026 {data.user.firstName} {data.user.lastName}. All rights reserved.
     </footer>
   );
 }
--- app/components/header/page.tsx
+++ app/components/header/page.tsx
@@ -1,7 +1,9 @@
+import data from "@/app/master_data/data";
+
 export default function Header() {
   return (
     <nav className="fixed w-full top-0 bg-black/70 backdrop-blur z-50 px-6 py-4 flex justify-between">
-      <p className="font-mono text-green-400">$ sahatsawat</p>
+      <p className="font-mono text-green-400">$ {data.user.firstName} {data.user.lastName}</p>
 
       <div className="flex gap-6 text-sm">
         <a href="#projects">projects</a>
